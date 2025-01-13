b46792da9c7342c4b46ae866e8bbbb03b80c4106
Jun Yan <junyan2000@hotmail.com>
2025-01-13 18:05:15 -0500

    useragent

--- YouTubeWMC/Form1.cs
+++ YouTubeWMC/Form1.cs
@@ -16,7 +16,7 @@ namespace YouTubeWMC
         private void webView21_CoreWebView2InitializationCompleted(object sender, CoreWebView2InitializationCompletedEventArgs e)
         {
             this.webView21.CoreWebView2.Settings.UserAgent =
-                "Mozilla/5.0 (SMART-TV) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246";
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 smarttv";
             this.webView21.Source = new System.Uri("https://youtube.com/tv", System.UriKind.Absolute);
         }
 
